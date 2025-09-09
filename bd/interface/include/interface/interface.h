05115b7c2df452ee34b5bfa8e72fa24a1980ce11
Saveliy <dedhek@mail.ru>
2025-09-09 17:19:11 +0300

    feat-[#STUDPOIP-48]:add interface class

--- bd/interface/include/interface/interface.h
+++ bd/interface/include/interface/interface.h
@@ -1,6 +1,15 @@
 #ifndef BD_INTERFACE_H
 #define BD_INTERFACE_H
-
+#include <vector>
+class interface {
+public:
+    virtual ~interface() = default;
+    virtual bool disconnect();
+    virtual bool isconnected();
+    virtual void write();
+    virtual void read();
+private:
+};
 
 
 #endif //BD_INTERFACE_H
