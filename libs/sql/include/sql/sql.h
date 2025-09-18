43eaaa551f11743f5cd06c99fa4540a31a43ee2a
Saveliy <dedhek@mail.ru>
2025-09-18 14:13:41 +0300

    feat-[#STUDPOIP-48]:add some methods sql db

--- libs/sql/include/sql/sql.h
+++ libs/sql/include/sql/sql.h
@@ -1,3 +1,20 @@
-//
-// Created by saveliy on 09.09.2025.
-//
+#ifndef SQL_H
+#define SQL_H
+#include "interface.h"
+#include "sqlite3.h"
+class SQL:DataBase {
+public:
+    SQL();
+    ~SQL() override;
+    bool connect() override;
+    bool disconnect() override;
+    bool isconnected() override;
+    bool writeUserFile(const std::string& name, const std::string& fileName) override;
+    std::string readUserFile(const std::string& name) override;
+private:
+    bool connected=false;
+    sqlite3* db;
+    std::string dbPath;
+};
+
+#endif
--- libs/sql/source/sql.cpp
+++ libs/sql/source/sql.cpp
@@ -1,3 +1,55 @@
-//
-// Created by saveliy on 09.09.2025.
-//
+#include "sql.h"
+#include <iostream>
+const std::string mainDataPath="DataBaseSql";
+SQL::SQL() :connected(false),db(nullptr),dbPath(mainDataPath){}
+SQL::~SQL() =default;
+bool SQL::connect() {
+    if (sqlite3_open(dbPath.c_str(), &db) != SQLITE_OK) {
+        std::cerr << "Ошибка открытия базы данных: " << sqlite3_errmsg(db) << std::endl;
+        db = nullptr;
+    } else {
+        connected = true;
+        std::cout << "База данных \"" << dbPath << "\" успешно открыта/создана." << std::endl;
+        return true;
+    }
+    return false;
+}
+bool SQL::disconnect() {
+    if (db) {
+        sqlite3_close(db);
+        std::cout << "База данных \"" << dbPath << "\" закрыта." << std::endl;
+        return true;
+    }
+    return false;
+}
+bool SQL::isconnected() {
+    return connected;
+}
+bool SQL::writeUserFile(const std::string &name, const std::string &fileName) {
+        if (!db) {
+            std::cerr << "База данных не подключена." << std::endl;
+            return false;
+        }
+
+        std::string sql = "CREATE TABLE IF NOT EXISTS Users ("
+                          "Name TEXT NOT NULL,"
+                          "File BLOB NOT NULL);";
+        const char* sql_query = "INSERT INTO Users (Name, File) VALUES (name, fileName);";
+        char* errMsg = nullptr;
+        sqlite3_stmt *stmt = nullptr;
+        int rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &errMsg);
+         std::string cuurentName=name;
+        int rc1 = sqlite3_bind_text(stmt, 1,cuurentName.c_str(), -1, SQLITE_STATIC);
+        if (rc != SQLITE_OK) {
+            std::cerr << "Ошибка создания таблицы: " << errMsg << std::endl;
+            sqlite3_free(errMsg);
+            return false;
+        } else {
+            std::cout << "Таблица 'Users' успешно создана (или уже существовала)." << std::endl;
+            return true;
+        }
+}
+std::string SQL::readUserFile(const std::string &name) {
+    return "";
+}
+
