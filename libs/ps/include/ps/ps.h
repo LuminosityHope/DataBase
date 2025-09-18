6d82f281c711fdab12b865cece554cbbee07fc4a
Saveliy <dedhek@mail.ru>
2025-09-18 12:40:56 +0300

    feat-[-48]:add dataBase with FS

--- libs/ps/include/ps/ps.h
+++ libs/ps/include/ps/ps.h
@@ -1,3 +1,16 @@
-//
-// Created by saveliy on 09.09.2025.
-//
+#include <iostream>
+#include "interface.h"
+class PS:public DataBase
+{
+public:
+    PS();
+    ~PS() override;
+    bool connect() override;
+    bool disconnect() override;
+    bool isconnected() override;
+    bool writeUserFile(const std::string& name, const std::string& fileName) override;
+    std::string readUserFile(const std::string& name) override;
+private:
+    bool connected=false;
+    std::filesystem::path mainDataBasePath;
+};
--- libs/ps/source/ps.cpp
+++ libs/ps/source/ps.cpp
@@ -1,3 +1,48 @@
-//
-// Created by saveliy on 09.09.2025.
-//
+#include "ps.h"
+
+const std::string mainDataPath="DataBaseFs";
+PS::PS():connected(false),mainDataBasePath(mainDataPath){}
+PS::~PS() {
+    if (connected) {
+    PS::disconnect();
+    }
+}
+bool PS::connect() {
+    connected=true;
+    std::filesystem::create_directories(mainDataBasePath);
+    return true;
+}
+bool PS::disconnect() {
+    connected=false;
+    return true;
+}
+bool PS::isconnected()  {
+    return connected;
+}
+bool PS::writeUserFile(const std::string &name, const std::string &fileName) {
+    if (!isconnected()) {
+        return false;
+    }
+    try {
+        std::filesystem::path folderName=mainDataBasePath/name;
+        std::filesystem::create_directories(folderName);
+
+        std::filesystem::path sourcePath=fileName;
+        std::filesystem::path destPath=folderName/sourcePath.filename();
+        std::filesystem::copy_file(sourcePath, destPath,std::filesystem::copy_options::overwrite_existing);
+        return true;
+
+    }
+    catch (const std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+    return false;
+
+}
+std::string PS::readUserFile(const std::string &name) {
+    if (!connected) {
+        return "";
+    }
+    std::filesystem::path absolutePath=std::filesystem::absolute(mainDataBasePath/name);
+    return absolutePath.string();
+}
