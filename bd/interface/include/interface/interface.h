a060920d643e0168daa90e06e7b3c22ecea3dd39
Saveliy <dedhek@mail.ru>
2025-09-24 10:42:54 +0300

    fix-[#STUDPOIP-48]:fix structure

--- bd/interface/include/interface/interface.h
+++ bd/interface/include/interface/interface.h
@@ -1,10 +1,12 @@
 #ifndef BD_INTERFACE_H
 #define BD_INTERFACE_H
+
 #include <filesystem>
 #include <fstream>
-#include <vector>
 
-enum class DataType {FileSystem,SQL};
+enum class DataType {
+    SQL,FileSystem
+};
 class DataBase {
 public:
     DataBase()=default;
@@ -14,5 +16,7 @@ public:
     virtual bool isconnected()=0;
     virtual bool writeUserFile(const std::string& name, const std::string& fileName)=0;
     virtual std::string readUserFile(const std::string& name)=0;
+    std::unique_ptr<DataBase> create(DataType type);
+
 };
 #endif //BD_INTERFACE_H
