ae2aaeb5075b47500ad66a4d399c6ac8b8b9e39a
Saveliy <dedhek@mail.ru>
2025-09-26 16:23:16 +0300

    add-[#STUDPOIP-48]:added error handling

--- main/main.cpp
+++ main/main.cpp
@@ -5,9 +5,15 @@
 
 #include <iostream>
 #include <string>
-
+std::map<Error, std::string> errorDescription = {
+    {Error::successConnect, "Success connect"}, {Error::failedToConnect, "You are not signed"},
+    {Error::successDisconnect,"Success disconnect"},{Error::failed,"Ошибка"},
+    {Error::successWriteToFile,"Your file added to dataBase"},{Error::failedWrite,"File has not been added "},
+    {Error::successCreateDB,"BD successfuly created"},{Error::failedCreateDB,"Fail created BD"}
+};
 typedef DataBase*(*CreateDataBase)();
 int main() {
+
     std::string type;
     std::string jsonPath="package.json";
     std::ifstream ifs(jsonPath);
@@ -24,9 +30,9 @@ int main() {
         std::cerr << "Ошибка парсинга JSON: " << e.what() << std::endl;
         return 1;
     }
-    std::string libraryPath="lib"+type+".so";
+     std::string libraryPath="lib"+type+".so";
 
-    void* libraryHandle=dlopen(libraryPath.c_str(), RTLD_LAZY);
+    void *libraryHandle= dlopen(libraryPath.c_str(), RTLD_LAZY);
     if (!libraryHandle) {
         fprintf(stderr, "dlopen() error: %s\n", dlerror());
         exit(1);
@@ -38,22 +44,29 @@ int main() {
         dlclose(libraryHandle);
         return {};
     }
-    auto db=createDataBaseFunc();
+     auto db=createDataBaseFunc();
     size_t number;
-    std::cout<<"Actions with DB"<<std::endl;
+    std::cout<<"Actions with DB "<<std::endl;
     int helpAction;
     do {
-        std::cout<<"Enter 1-Connect to DataBase"<<std::endl;
-        std::cout<<"Enter 2-Write to DataBase"<<std::endl;
-        std::cout<<"Enter 3-Read from DataBase"<<std::endl;
-        std::cout<<"Enter 4-Disconnect from DataBase"<<std::endl;
+        std::cout<<"Enter 1: Connect to DataBase"<<std::endl;
+        std::cout<<"Enter 2: Write to DataBase"<<std::endl;
+        std::cout<<"Enter 3: Read from DataBase"<<std::endl;
+        std::cout<<"Enter 4: Disconnect from DataBase"<<std::endl;
+        std::cout<<"Enter 5: Exit"<<std::endl;
         std::cin>>helpAction;
         switch(helpAction) {
             case 1: {
-                db->connect();
+                Error message=db->connect();
+                std::cerr<<errorDescription.at(message)<<std::endl;
                 break;
             }
             case 2: {
+                if (!db->isconnected()) {
+                    Error message=Error::failedToConnect;
+                    std::cerr<<errorDescription.at(message)<<std::endl;
+                    break ;
+                }
                 std::string name;
                 std::cout<<"Enter your name:";
                 std::cin>>name;
@@ -61,10 +74,16 @@ int main() {
                 std::cout<<"Enter your file path:";
                 std::cin.ignore();
                 std::getline(std::cin,filename);
-                db->writeUserFile(name, filename);
+                Error message1=db->writeUserFile(name, filename);
+                    std::cerr<<errorDescription.at(message1)<<std::endl;
                 break;
             }
             case 3: {
+                if (!db->isconnected()) {
+                    Error message=Error::failedToConnect;
+                    std::cerr<<errorDescription.at(message)<<std::endl;
+                    break;
+                }
                 std::string name;
                 std::cout<<"Enter your name:";
                 std::cin>>name;
@@ -72,10 +91,15 @@ int main() {
                 break;
             }
             case 4: {
-                db->disconnect();
+                Error error=db->disconnect();
+                std::cerr<<errorDescription.at(error)<<std::endl;
+                break;
+            }
+                case 5: {
+                std::cout<<"Exit"<<std::endl;
                 break;
             }
         }
-    }while(helpAction!=4);
+    }while(helpAction!=5);
     return 0;
 }
